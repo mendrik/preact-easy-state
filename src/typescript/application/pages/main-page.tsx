844e9106d611fc1359426b0a5a86be64b6593ecc
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-17 22:32:46 +0300

    remove postcss-cssnext crap

--- src/typescript/application/pages/main-page.tsx
+++ src/typescript/application/pages/main-page.tsx
@@ -100,7 +100,93 @@ export class MainPage extends QuillComponent {
                             Anchiceratops<br/>
                             Anchiornis<br/>
                         </div>
-                        <div>Right</div>
+                        <div>
+                            Replica of an Allosaurus skeleton.<br/>
+                            Aachenosaurus – a piece of petrified wood<br/>
+                            Aardonyx<br/>
+                            Abdallahsaurus – nomen nudum, probably Giraffatitan<br/>
+                            Abelisaurus<br/>
+                            Abrictosaurus<br/>
+                            Abrosaurus<br/>
+                            Abydosaurus<br/>
+                            Acanthopholis<br/>
+                            Achelousaurus<br/>
+                            Acheroraptor<br/>
+                            Achillesaurus<br/>
+                            Achillobator<br/>
+                            Acristavus<br/>
+                            Acrocanthosaurus<br/>
+                            Acrotholus<br/>
+                            Actiosaurus – a choristoderan<br/>
+                            Adamantisaurus<br/>
+                            Adasaurus<br/>
+                            Adelolophus<br/>
+                            Adeopapposaurus<br/>
+                            Aegyptosaurus<br/>
+                            Aeolosaurus<br/>
+                            Aepisaurus<br/>
+                            Aepyornithomimus<br/>
+                            Aerosteon<br/>
+                            Aetonyx – possible junior synonym of Massospondylus<br/>
+                            Afromimus<br/>
+                            Afrovenator<br/>
+                            Agathaumas - possible synonym of Triceratops<br/>
+                            Aggiosaurus – a metriorhynchid crocodilian<br/>
+                            Agilisaurus<br/>
+                            Agnosphitys<br/>
+                            Agrosaurus – probably a junior synonym of Thecodontosaurus<br/>
+                            Agujaceratops<br/>
+                            Agustinia<br/>
+                            Ahshislepelta<br/>
+                            Airakoraptor - nomen nudum<br/>
+                            Ajancingenia - synonym of Heyuannia<br/>
+                            Ajkaceratops<br/>
+                            Alamosaurus<br/>
+                            Alaskacephale<br/>
+                            Albalophosaurus<br/>
+                            Albertaceratops<br/>
+                            Albertadromeus<br/>
+                            Albertavenator<br/>
+                            Albertonykus<br/>
+                            Albertosaurus<br/>
+                            Albinykus[1]<br/>
+                            Albisaurus – a non-dinosaurian reptile<br/>
+                            Alcovasaurus<br/>
+                            Alectrosaurus<br/>
+                            Aletopelta<br/>
+                            Algoasaurus<br/>
+                            Alioramus<br/>
+                            Artist's reconstruction of Amargasaurus.<br/>
+                            Aliwalia – junior synonym of Eucnemesaurus<br/>
+                            Allosaurus<br/>
+                            Almas<br/>
+                            Alnashetri<br/>
+                            Alocodon<br/>
+                            Altirhinus<br/>
+                            Altispinax<br/>
+                            Alvarezsaurus<br/>
+                            Alwalkeria<br/>
+                            Alxasaurus<br/>
+                            Amargasaurus<br/>
+                            Amargastegos<br/>
+                            Amargatitanis<br/>
+                            Amazonsaurus<br/>
+                            Ammosaurus - possible junior synonym of Anchisaurus<br/>
+                            Ampelosaurus<br/>
+                            Amphicoelias<br/>
+                            Amphicoelicaudia – nomen nudum; possibly Huabeisaurus<br/>
+                            Amphisaurus – preoccupied name, now known as Anchisaurus<br/>
+                            Amtocephale<br/>
+                            Amtosaurus – possibly Talarurus<br/>
+                            Amurosaurus<br/>
+                            Amygdalodon<br/>
+                            Anabisetia<br/>
+                            Anasazisaurus<br/>
+                            Anatosaurus – junior synonym of Edmontosaurus<br/>
+                            Anatotitan – junior synonym of Edmontosaurus<br/>
+                            Anchiceratops<br/>
+                            Anchiornis<br/>
+                        </div>
                     </HorizontalSplit>
                 </ScrollPane>
                 <footer class="footer"/>
