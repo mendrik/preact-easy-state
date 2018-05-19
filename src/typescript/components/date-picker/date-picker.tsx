55acbae372bed66082e3683dfb25163fc85ff3d8
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-19 21:47:24 +0300

    add document click annotation

--- src/typescript/components/date-picker/date-picker.tsx
+++ src/typescript/components/date-picker/date-picker.tsx
@@ -5,7 +5,8 @@ import {FormProps} from '../forms/types'
 import {format as formatDate} from 'fecha'
 import {observable} from '@nx-js/observer-util'
 import './date-picker.pcss'
-import {Modal} from '../modal/modal'
+import os from 'obj-str'
+import {DocumentClick} from '../../decorators/document-click'
 
 interface DatePickerProps extends FormProps<Date> {
     error?: string
@@ -35,24 +36,29 @@ export class DatePicker extends QuillComponent<DatePickerProps> {
         this.model.dropDownVisible = !this.model.dropDownVisible
     }
 
-    close = () => {
+    @DocumentClick((dp: DatePicker) => dp.model.dropDownVisible)
+    closeDropDown() {
         this.model.dropDownVisible = false
     }
 
     render({children, name, changes, value, format, ...props}) {
         return (
-            <div class="control has-icons-right date-picker">
+            <div class={os({
+                'control has-icons-right date-picker dropdown': 1,
+                'is-active': this.model.dropDownVisible
+            })}>
                 <input type="text"
                        class="input is-small date"
                        name={name}
                        value={formatDate(value, format)}/>
-                <span class="icon is-small is-right" onClick={this.iconClick}>
+                <span class="icon is-small is-right dropdown-trigger" onClick={this.iconClick}>
                     <i class="mdi mdi-calendar-range"/>
                 </span>
-                <Modal visible={this.model.dropDownVisible}
-                       onClose={this.close}>
-                    <div>Calendar</div>
-                </Modal>
+                <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                    <div class="dropdown-content">
+                        Calendar goes<br/>here
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ src/typescript/decorators/document-click.ts
@@ -0,0 +1,24 @@
+import {addToCleanupQueue, addToMountQueue} from '../util/construct'
+import {QuillComponent, QuillComponentClass} from '../util/quill-component'
+import {observe, unobserve} from '@nx-js/observer-util'
+
+export const DocumentClick = <T extends QuillComponent>
+    (onWhen: (comp: T) => boolean) => (proto: QuillComponentClass, method: string) => {
+
+    addToMountQueue(proto, (instance: T) => {
+        observe(() => onWhen(instance), {
+            scheduler: () => {
+                if (onWhen(instance)) {
+                    const handler = (ev: MouseEvent) => {
+                        if (!instance.base.contains(ev.target as Node)) {
+                            instance[method](ev)
+                            document.removeEventListener('mousedown', handler)
+                        }
+                    }
+                    document.addEventListener('mousedown', handler)
+                }
+            }
+        })
+        addToCleanupQueue(instance, () => unobserve(onWhen))
+    })
+}
