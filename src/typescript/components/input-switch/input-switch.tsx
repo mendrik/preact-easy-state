75f028bfdba248d7c52195a8c721a3ea7f168536
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-27 11:45:18 +0300

    switch css started

--- src/typescript/components/input-switch/input-switch.tsx
+++ src/typescript/components/input-switch/input-switch.tsx
@@ -3,6 +3,7 @@ import {View} from '../../decorators/view'
 import {QuillComponent} from '../../util/quill-component'
 import {FormProps} from '../forms/types'
 import './input-switch.pcss'
+import {cls} from '../../util/utils'
 
 export interface InputSwitchProps extends FormProps<boolean> {
     onLabel?: string
@@ -13,13 +14,27 @@ export interface InputSwitchProps extends FormProps<boolean> {
 export class InputSwitch extends QuillComponent<InputSwitchProps> {
 
     static defaultProps = {
-        onLabel: 'on',
+        onLabel: 'this is on',
         offLabel: 'off'
     }
 
-    render({children, changes, ...props}) {
+    componentDidMount() {
+        const offLabelWidth = this.base.querySelector('.off-label').getBoundingClientRect().width
+        const onLabelWidth = this.base.querySelector('.on-label').getBoundingClientRect().width
+        this.base.style.setProperty('--off-width', `${offLabelWidth}px`)
+        this.base.style.setProperty('--on-width', `${onLabelWidth}px`)
+    }
+
+    render({children, changes, value, onLabel, offLabel, ...props}) {
         return (
             <div class="control has-icons-right boolean-input">
+                <div class={cls('switch-wrapper', {on: value})}>
+                    <div class="slider">
+                        <div class="on-label">{onLabel}</div>
+                        <div class="toggle"/>
+                        <div className="off-label">{offLabel}</div>
+                    </div>
+                </div>
                 {children}
             </div>
         )
