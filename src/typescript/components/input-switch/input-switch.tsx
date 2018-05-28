957756dd7dfa12890317d6def9ffb8b37452b5bd
Andreas Prudzilko <mendrik76@gmail.com>
2018-05-28 14:01:38 +0300

    fix up switch

--- src/typescript/components/input-switch/input-switch.tsx
+++ src/typescript/components/input-switch/input-switch.tsx
@@ -3,47 +3,50 @@ import {View} from '../../decorators/view'
 import {QuillComponent} from '../../util/quill-component'
 import {FormProps} from '../forms/types'
 import './input-switch.pcss'
-import {cls} from '../../util/utils'
 import {CustomEvent} from '../../decorators/custom-event'
+import {cls} from '../../util/utils'
 
 export interface InputSwitchProps extends FormProps<boolean> {
     onLabel?: string
     offLabel?: string
 }
 
-@View
-export class InputSwitch extends QuillComponent<InputSwitchProps> {
+export interface InputSwitchState {
+    loaded: boolean
+}
 
+@View
+export class InputSwitch extends QuillComponent<InputSwitchProps, InputSwitchState> {
+
+    label: HTMLLabelElement
     slider: HTMLDivElement
-    loaded = false
 
     static defaultProps = {
         onLabel: 'enabled',
-        offLabel: 'disabled'
+        offLabel: 'off'
     }
 
-    @CustomEvent('animationstart')
-    cssReady() {
-        const {slider} = this
-        const minWidth = slider.getBoundingClientRect().width
-        this.setState({minWidth})
-        this.loaded = true
+    hasCss = () => {
+        const {slider, label} = this
+        label.style.setProperty('--max-width', `${slider.offsetWidth}px`)
+        this.setState({loaded: true})
     }
 
-    render({children, changes, value, onLabel, offLabel, ...props}, {minWidth = 0}) {
+    render({children, changes, value, onLabel, offLabel, ...props}, {loaded = false}) {
         return (
-            <div class={cls('control boolean-input', {loaded: this.loaded})}
-                 style={`--min-width: ${minWidth}px`}
-                 onClick={() => changes(!value)}>
-                <div class={cls('switch-wrapper', {off: !value})}>
+            <div class={cls('control boolean-input', {loaded})} onAnimationEnd={this.hasCss} >
+                <label class="switch" ref={l => this.label = l}>
+                    <input type="checkbox" onClick={() => changes(!value)} checked={value}/>
                     <div class="slider" ref={r => this.slider = r}>
-                        <div class="on-label"><span>{onLabel}</span></div>
+                        <div class="label-off">{offLabel}</div>
                         <div class="toggle"/>
-                        <div class="off-label"><span>{offLabel}</span></div>
+                        <div class="label-on">{onLabel}</div>
                     </div>
-                </div>
+                </label>
                 {children}
             </div>
         )
     }
+
 }
+
