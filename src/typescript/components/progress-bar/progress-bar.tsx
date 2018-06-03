586888fd669e15423ed520f6324bead1230bff2d
Andreas Prudzilko <mendrik76@gmail.com>
2018-06-03 12:04:10 +0300

    fix ios progress bar

--- src/typescript/components/progress-bar/progress-bar.tsx
+++ src/typescript/components/progress-bar/progress-bar.tsx
@@ -2,6 +2,15 @@ import {Component, h} from 'preact'
 import './progress-bar.pcss'
 
 export class ProgressBar extends Component {
+
+    componentDidMount() {
+        document.documentElement.classList.add('progress')
+    }
+
+    componentWillUnmount() {
+        document.documentElement.classList.remove('progress')
+    }
+
     render() {
         return <div class="progress-runner animated fadeIn"/>
     }
