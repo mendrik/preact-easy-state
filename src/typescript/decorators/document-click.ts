c05197ebdae751fcb41888cee58e213cf4624da8
Andreas Prudzilko <mendrik76@gmail.com>
2018-06-09 18:42:20 +0300

    tweak document click

--- src/typescript/decorators/document-click.ts
+++ src/typescript/decorators/document-click.ts
@@ -1,22 +1,21 @@
 import {addToCleanupQueue, addToMountQueue} from '../util/construct'
 import {QuillComponent, QuillComponentClass} from '../util/quill-component'
 import {observe, unobserve} from '@nx-js/observer-util'
+import {hasPointers} from '../util/events'
 
 export const DocumentClick = <T extends QuillComponent>
     (onWhen: (comp: T) => boolean) => (proto: QuillComponentClass, method: string) => {
-
+    const event = hasPointers ? 'pointerdown' : 'touchstart'
     addToMountQueue(proto, (instance: T) => {
         observe(() => {
             if (onWhen(instance)) {
                 const handler = (ev: MouseEvent) => {
                     if (!instance.base.contains(ev.target as Node)) {
                         instance[method](ev)
-                        document.removeEventListener('mousedown', handler)
-                        document.removeEventListener('touchstart', handler)
+                        document.removeEventListener(event, handler)
                     }
                 }
-                document.addEventListener('mousedown', handler)
-                document.addEventListener('touchstart', handler)
+                document.addEventListener(event, handler)
             }
         })
         addToCleanupQueue(instance, () => unobserve(onWhen))
